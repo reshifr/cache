07ccbc008fccc29d28fa9218d4a5206ad5179e9e
Renol P. H <reshifr@gmail.com>
2023-06-03 21:12:27 +0700

    refactor: rdrand add new functional methods

--- include/mind/crypt/rdrand.h
+++ include/mind/crypt/rdrand.h
@@ -21,31 +21,78 @@ public:
 };
 
 /**
- * \brief RDRAND random number generator
- * \tparam L Block length in bytes
- * \note `L` must be greater than 0.
+ * \brief RDRAND random number generator base class
  */
-template <u L>
-class rdrand {
+class rdrand_base {
 public:
-  using blk_t = sblk<L>;
-
   /**
-   * \brief Generate secure random block
+   * \brief Generate dynamic secure random block
+   * \param len Block length in bytes
    * \return A secure random block
    * \throws rdrand_error Error in generating random number
+   * \throws rdrand_error RDRAND instruction is not supported
    */
-  blk_t operator()(void) const {
-    blk_t rn;
+  dblk operator()(u len) {
+    dblk rn(len);
     try {
-      CryptoPP::RDRAND rng;
-      rng.GenerateBlock(rn.data(), L);
+      m_rng.GenerateBlock(rn.data(), len);
+    } catch(...) {
+      throw rdrand_error();
+    }
+    return rn;
+  }
+
+  /**
+   * \brief Generate secure random in-place block
+   * \tparam In Input iterator type
+   * \param in Iterator to the beginning of the block
+   * \param len Block length in bytes
+   * \throws rdrand_error Error in generating random number
+   * \throws rdrand_error RDRAND instruction is not supported
+   */
+  template <typename In>
+  void operator()(In in, u len) {
+    try {
+      m_rng.GenerateBlock(std::addressof(*in), len);
+    } catch(...) {
+      throw rdrand_error();
+    }
+  }
+
+protected:
+  CryptoPP::RDRAND m_rng;
+};
+
+/**
+ * \brief RDRAND random number generator with `L` greater than 0
+ * \tparam L Block length in bytes
+ * \note If `L` is 0, then the base class functors are used.
+ */
+template <u L=u(0)>
+class rdrand : public rdrand_base {
+public:
+  /**
+   * \brief Generate static secure random block
+   * \return A secure random block
+   * \throws rdrand_error Error in generating random number
+   * \throws rdrand_error RDRAND instruction is not supported
+   */
+   sblk<L> operator()(void) {
+    sblk<L> rn;
+    try {
+      m_rng.GenerateBlock(rn.data(), L);
     } catch(...) {
       throw rdrand_error();
     }
     return rn;
   }
 };
+
+/**
+ * \brief RDRAND random number generator with `L` is 0
+ */
+template <>
+class rdrand<u(0)> : public rdrand_base {};
 
 } // namespace mind
 
